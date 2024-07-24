12704eaa046ccd9115d2d42a75d8b51b5f133c12
Duy Nhan <duynhannguyenn@gmail.com>
2024-07-24 22:50:41 +0700

    feat: set up create variant form

--- app/dashboard/products/product-variants.tsx
+++ app/dashboard/products/product-variants.tsx
@@ -8,7 +8,23 @@ import {
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { ProductVariantsImagesTags } from "@/lib/infer-type";
-
+import { variantsImages } from "@/server/schema";
+import { VariantSchema } from "@/types/variant-schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useAction } from "next-safe-action/hooks";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
 const ProductVariant = ({
   editMode,
   productId,
@@ -20,17 +36,102 @@ const ProductVariant = ({
   variant?: ProductVariantsImagesTags;
   children: React.ReactNode;
 }) => {
+  const form = useForm<z.infer<typeof VariantSchema>>({
+    resolver: zodResolver(VariantSchema),
+    defaultValues: {
+      tag: [],
+      variantImages: [],
+      color: "#000000",
+      editMode,
+      id: undefined,
+      productId,
+      productType: "Sach Lich Su",
+    },
+  });
+
+  const onSubmit = () => {};
+
   return (
     <Dialog>
       <DialogTrigger>{children}</DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogTitle>
+            {editMode ? "Edit Mode" : "Create"} your variant{" "}
+          </DialogTitle>
           <DialogDescription>
-            This action cannot be undone. This will permanently delete your
-            account and remove your data from our servers.
+            Manage your product variants here. You can add tags, images and more
           </DialogDescription>
         </DialogHeader>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="productType"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variant Title</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Pick a title for your variant "
+                      {...field}
+                    />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="color"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variant Color</FormLabel>
+                  <FormControl>
+                    <Input type="color" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="tag"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variant Tags</FormLabel>
+                  <FormControl>
+                    <InputTags />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="variantImages"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Variant Images</FormLabel>
+                  <FormControl>
+                    <InputTags />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {editMode && variant && (
+              <Button type="button"> Delete Variant </Button>
+            )}
+            <Button type="submit">
+              {editMode ? "Update Variant" : "Create variant"}
+            </Button>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   );
